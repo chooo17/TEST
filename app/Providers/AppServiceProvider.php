0d467f4393a291e0014008f154fffd360597cc63
chooo17 <thezuama@gmail.com>
2026-05-02 22:18:50 +0700

    fix PermissionRegistrar namespace

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Spatie\Permission\PermissionRegistrar; // 🔥 namespace yang benar
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,10 +14,12 @@ class AppServiceProvider extends ServiceProvider
     }
 
     public function boot(): void
-{
-    if ($this->app->environment('production')) {
-        URL::forceScheme('https');
+    {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
+        // 🔥 Clear permission cache
+        app(PermissionRegistrar::class)->forgetCachedPermissions();
     }
-     app(PermissionRegistrar::class)->forgetCachedPermissions();
-}
 }
