d80689f407bdd6501223aebaf62bfac5ec9170e7
chooo17 <thezuama@gmail.com>
2026-05-06 21:45:45 +0700

    +laporan harian

--- routes/admin.php
+++ routes/admin.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\LaporanController;
 
 Route::middleware(['auth', 'role:admin|superadmin'])->group(function () {
     Route::get('/laporan', [LaporanController::class, 'index'])->name('laporan');
+    Route::get('/laporan/harian', [LaporanController::class, 'harian'])->name('laporan.harian');
 });
 
 Route::middleware(['auth', 'role:admin'])->group(function () {
