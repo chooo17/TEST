8c87b715bfb476ac35a5ea567f898dee27d8300f
chooo17 <thezuama@gmail.com>
2026-05-07 19:35:39 +0700

    fix padding kasir

--- resources/js/Components/Menu.jsx
+++ resources/js/Components/Menu.jsx
@@ -94,9 +94,9 @@ export default function Menu({ onAdd, openCart, products = [], categories = [] }
                 onMouseUp={stopDrag}
                 onMouseLeave={stopDrag}
                 onMouseMove={handleMouseMove}
-                className="flex-1 overflow-y-auto pr-3 pl-1 pb-2 no-scrollbar cursor-grab active:cursor-grabbing select-none overscroll-contain"
+                className="flex-1 overflow-y-auto px-3 pb-2 no-scrollbar cursor-grab active:cursor-grabbing select-none overscroll-contain"
             >
-                <div className="grid px-1 py-2 grid-cols-2 sm:grid-cols-4 lg:grid-cols-4 gap-3 min-w-0">
+                <div className="grid py-2 grid-cols-2 sm:grid-cols-4 lg:grid-cols-4 gap-3 min-w-0">
                     {filteredProducts.length > 0 ? (
                         filteredProducts.map((product) => (
                             <div
